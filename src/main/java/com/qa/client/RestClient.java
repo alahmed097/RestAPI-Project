3b1d70f21c02fc90af917e070f053c56a3636d49
Ali  Ahmed <AliAhmed@Alis-Air.home>
2020-03-12 15:06:24 -0400

    Final Commit

---
+++ src/main/java/com/qa/client/RestClient.java
@@ -0,0 +1,44 @@
+package com.qa.client;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.http.Header;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+public class RestClient {
+
+	//1. GET method 
+	public void get(String url) throws ClientProtocolException, IOException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(url); //http get request
+		CloseableHttpResponse closeableHttpResponse =  httpClient.execute(httpget); //hit the get url
+		
+		//a. Status Code:
+		int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+		System.out.println("Status Code----> "+statusCode);
+		
+		//b. Json String:
+		String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+		
+		JSONObject responsejson = new JSONObject(responseString);
+		System.out.println("Response JSON from API ---> "+ responsejson);
+		
+		//c. All headers
+		Header[] headersArray = closeableHttpResponse.getAllHeaders();
+		HashMap<String, String> allHeaders = new HashMap<String, String>();
+		for(Header header : headersArray) {
+			allHeaders.put(header.getName(), header.getValue());
+		}
+		System.out.println("Headers Array ---> "+ allHeaders);
+	}
+	
+	
+	
+}
